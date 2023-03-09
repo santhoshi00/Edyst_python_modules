5bd26e7ba69d369da95b55cf9ea8db107b36c30a
santhoshi00 <107908927+santhoshi00@users.noreply.github.com>
2023-03-09 22:31:34 +0530

    Create Going low.py

---
+++ The Basics/Intro to Strings/Going low.py
@@ -0,0 +1,7 @@
+"""Given a string stored in the variable my_str, convert all the letters of this string to lower case and then print it.
+
+Complete the given program."""
+
+my_str='HeLLoEdystPROgRammer'
+
+print(my_str.lower())
