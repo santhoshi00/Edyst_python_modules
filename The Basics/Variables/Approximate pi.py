c7a1b87a454fac03177ba3ac67509c7ca8ec5d88
santhoshi00 <107908927+santhoshi00@users.noreply.github.com>
2023-03-09 22:18:45 +0530

    Create Approximate pi.py

---
+++ The Basics/Variables/Approximate pi.py
@@ -0,0 +1,11 @@
+"""Write a program to store the value of pi till 5 decimal places in a variable and print it.
+
+Example Output:
+
+The approximate value is 3.14159
+
+Note: Do not calculate the value of pi. Simply print the above value
+print('3.14159')"""
+
+float=3.14159
+print(float)
