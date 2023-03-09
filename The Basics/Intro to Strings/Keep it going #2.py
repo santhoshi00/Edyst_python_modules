97af10e8b34c89639e000ddf3a765c07027bb972
santhoshi00 <107908927+santhoshi00@users.noreply.github.com>
2023-03-09 22:30:55 +0530

    Create Keep it going #2.py

---
+++ The Basics/Intro to Strings/Keep it going #2.py
@@ -0,0 +1,11 @@
+"""In a new language, we speak by repeating each word the same number of times as the length of the string. For the given word, print the word in the new language
+
+Complete the given program"""
+
+word='Hello'
+
+print(word)
+print(word)
+print(word)
+print(word)
+print(word)
