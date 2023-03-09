6d6113f3b60daf936739196faf1b4d836630097d
santhoshi00 <107908927+santhoshi00@users.noreply.github.com>
2023-03-09 17:34:15 +0530

    Create Print Name of Friends.py

---
+++ Print Name of Friends.py
@@ -0,0 +1,6 @@
+"""Using the format method, print the following statement by substituting the name of friends:
+
+OUTPUT:
+Alex, Laxmi and Jay went to the party"
+
+print("{0}, {1} and {2} went to the {3}".format("Alex","Laxmi","Jay","party"))
