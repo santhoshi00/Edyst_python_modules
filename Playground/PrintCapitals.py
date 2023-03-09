8bf9db17b84e1e1b5014c8502c29817c12fadf94
santhoshi00 <107908927+santhoshi00@users.noreply.github.com>
2023-03-09 17:38:30 +0530

    Create PrintCapitals.py

---
+++ Playground/PrintCapitals.py
@@ -0,0 +1,11 @@
+"""Using the concept of f-strings, print the following statement by substituting the name of countries and capitals:
+
+New Delhi is the capital of India
+Madrid is the capital of Spain
+"""
+capital="New Delhi"
+country="India"
+print(f"{capital} is the capital of {country}")
+capital="Madrid"
+country="Spain"
+print(f"{capital} is the capital of {country}")
