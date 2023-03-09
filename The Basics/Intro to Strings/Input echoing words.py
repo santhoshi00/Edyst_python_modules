786143bf2b7c3db5dca5aa8eed2421c4b2b9598e
santhoshi00 <107908927+santhoshi00@users.noreply.github.com>
2023-03-09 22:35:21 +0530

    Create Input echoing words.py

---
+++ The Basics/Intro to Strings/Input echoing words.py
@@ -0,0 +1,13 @@
+"""Given a word and a number, you must print it's echo. The echo is the repetition of the word for the given number of repetitions.
+
+Example Input
+
+Hello
+5
+Output
+
+HelloHelloHelloHelloHello"""
+
+word=input()
+n=int(input())
+print(str(word)*n)
