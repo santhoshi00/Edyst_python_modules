0936b04b716d7b6e593161fe3780a7dffd602e00
santhoshi00 <107908927+santhoshi00@users.noreply.github.com>
2023-03-09 22:32:55 +0530

    Create Input Integer in Python.py

---
+++ The Basics/Intro to Strings/Input Integer in Python.py
@@ -0,0 +1,12 @@
+"""Write a program that inputs the age of the user and prints it in the given format
+
+Example Input:
+
+33
+
+Output:
+
+Hi user, your age is 33. Cool!"""
+
+age=int(input())
+print("Hi user, your age is " + str(age)+ ". Cool!")
