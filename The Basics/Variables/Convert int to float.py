a61d22a31ae2ae4c3dc0181ac51a1466fa849d40
santhoshi00 <107908927+santhoshi00@users.noreply.github.com>
2023-03-09 22:23:03 +0530

    Create Convert int to float.py

---
+++ The Basics/Variables/Convert int to float.py
@@ -0,0 +1,11 @@
+"""A new upgrade of your favorite coding language has released. It is the 4th version. However, the industry likes to view it in decimal points. Thus 4 should be viewed as 4.0
+
+Store the value 4 in an an integer and then convert it to a float value. Print the float value.
+
+Example Output:
+
+4.0"""
+
+number=4
+decimal_num=float(number)
+print(decimal_num)
