7751a942e23491c3691cd3e71a30ec2f7869f60c
santhoshi00 <107908927+santhoshi00@users.noreply.github.com>
2023-03-09 22:26:31 +0530

    Create Up & Up.py

---
+++ The Basics/Intro to Strings/Up & Up.py
@@ -0,0 +1,8 @@
+"""The passport officer demands that your name must be in capital letters only. Help our the passport officer. Given a string name, convert all the letters to upper case and then print it
+
+Complete the given program."""
+
+name='HelLoEdystProgRammer'
+
+# your code below this line #
+print(name.upper())
