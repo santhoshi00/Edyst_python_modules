2f4ed03f28726c5eb203abf0adb6be4392771fdc
santhoshi00 <107908927+santhoshi00@users.noreply.github.com>
2023-03-09 22:34:32 +0530

    Create Input shoe and size.py

---
+++ The Basics/Intro to Strings/Input shoe and size.py
@@ -0,0 +1,13 @@
+"""Write a program the takes as input the shoe and size of the user. Print it in the given format
+
+Example Input
+
+Boots
+13
+Output
+
+User wears Boots - size is 13"""
+
+shoe=input()
+size=input()
+print("User wears " + str(shoe) + " - size is " + str(size))
