4c9ecf6b2ca7237f858d7a716e1b30b0af649ca7
santhoshi00 <107908927+santhoshi00@users.noreply.github.com>
2023-03-09 22:38:39 +0530

    Create Swap the integers.py

---
+++ The Basics/Operators & Precedence/Swap the integers.py
@@ -0,0 +1,18 @@
+"""You are given 2 numbers, num1 and num2. You must swap the values stored in both the variables.
+
+Complete the given code.
+
+Example Input
+
+2 5
+
+Output
+
+5 2"""
+
+num1 = 10
+num2 = 20
+
+temp=num1
+num1=num2
+num2=temp
