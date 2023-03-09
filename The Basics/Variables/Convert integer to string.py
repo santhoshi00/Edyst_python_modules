8f4e7bbbc8d0bdf09220dc81aa45714844de3126
santhoshi00 <107908927+santhoshi00@users.noreply.github.com>
2023-03-09 22:24:40 +0530

    Create Convert integer to string.py

---
+++ The Basics/Variables/Convert integer to string.py
@@ -0,0 +1,11 @@
+"""You are sending a message to your friend on whatsapp - there are some numbers in the message, but they must be a string.
+
+Store the number 98180101 in an integer. Convert it to a string and then print the string.
+
+Example Output:
+
+98180101"""
+
+int=98180101
+string_3=str(int)
+print(string_3)
