f0e30221a7560bb094fa6dbaa9aa64fc71de836b
santhoshi00 <107908927+santhoshi00@users.noreply.github.com>
2023-03-09 22:22:16 +0530

    Create Convert float to int.py

---
+++ The Basics/Variables/Convert float to int.py
@@ -0,0 +1,11 @@
+"""The value of gravitational acceleration on earth is approximated to 9.8
+
+Store this value in a variable called g. Convert g into an integer and store it in int_g. Print int_g
+
+Example Output:
+
+9"""
+
+g=9.8
+intger_g=int(g)
+print(intger_g)
