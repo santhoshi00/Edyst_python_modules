a7cee0f2df3d892a83725ec66af6c0f56b5fcab1
santhoshi00 <107908927+santhoshi00@users.noreply.github.com>
2023-03-09 22:32:15 +0530

    Create Input String in Python.py

---
+++ The Basics/Intro to Strings/Input String in Python.py
@@ -0,0 +1,13 @@
+"""Write a program that inputs the name of the user and prints it in the given format
+
+Example Input:
+
+Sachin
+
+Output:
+
+Hello Sachin, glad to meet you!"""
+
+name=input()
+
+print("Hello " + str(name)+", glad to meet you!")
