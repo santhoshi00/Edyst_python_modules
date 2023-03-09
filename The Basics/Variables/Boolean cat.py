0866a7b9842dce5cf3bf19745b28cca0f003ae71
santhoshi00 <107908927+santhoshi00@users.noreply.github.com>
2023-03-09 22:19:32 +0530

    Create Boolean cat.py

---
+++ The Basics/Variables/Boolean cat.py
@@ -0,0 +1,10 @@
+"""Write a program to store 2 boolean values in variables and then print them in the following manner:
+
+Some values are True
+Others are False
+And then, there is Schrodinger's cat
+"""
+
+print("Some values are True")
+print("Others are False")
+print("And then, there is Schrodinger's cat")
