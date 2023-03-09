a8d1f8749bb1536ee6ab1e5983d1378f60babaca
santhoshi00 <107908927+santhoshi00@users.noreply.github.com>
2023-03-09 22:12:09 +0530

    Create Your agent name.py

---
+++ Playground/Fun with Advances Concpets/Your agent name.py
@@ -0,0 +1,11 @@
+"""You are now a secret agent and to must generate an agent name. The agent name is the combination of the first, third and fifth letter of your name.
+
+Given a string name, print the agent name.
+
+Complete the given program"""
+
+Name='ReaLmEdystProgRammerhvvdvkjhoillhdsiolksdluvsdkjhlkdiufdjkkvdsoajckvuskcnkjasdvkknkdfjkvb'
+### your answer below this line ###
+
+agent_name=Name[0]+Name[2]+Name[4]
+print(agent_name)
