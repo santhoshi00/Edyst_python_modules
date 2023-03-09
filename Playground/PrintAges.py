f6ce02168489685b814781e280a428b0b36797a3
santhoshi00 <107908927+santhoshi00@users.noreply.github.com>
2023-03-09 17:37:50 +0530

    Create PrintAges.py

---
+++ Playground/PrintAges.py
@@ -0,0 +1,16 @@
+"""Using the concept of f-strings, print the following statement by substituting the ages:
+
+Riya's age is 5
+Mahesh is 10
+And their best friend, Arjun, is 12
+"""
+
+name="Riya"
+age = "5"
+print(f"{name}'s age is {age}")
+name="Mahesh"
+age="10"
+print(f"{name} is {age}")
+name="Arjun"
+age="12"
+print(f"And their best friend, {name}, is {age}")
