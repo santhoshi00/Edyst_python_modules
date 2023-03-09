2237302be135d17ac3762bd7d4dc41fe56e7bf50
santhoshi00 <107908927+santhoshi00@users.noreply.github.com>
2023-03-09 22:33:41 +0530

    Create Input Decimal.py

---
+++ The Basics/Intro to Strings/Input Decimal.py
@@ -0,0 +1,12 @@
+"""Write a program that takes as input the speed of a car (in decimal) and print it in the given format
+
+Example input:
+
+47.2
+
+Output
+
+This car is really fast - clocking 47.2 kmph"""
+
+decimal=input()
+print("This car is really fast - clocking " + str(decimal) +" kmph")
