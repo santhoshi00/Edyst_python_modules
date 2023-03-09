3742c860d7dd255aa8b023d4c0073d442f0d7726
santhoshi00 <107908927+santhoshi00@users.noreply.github.com>
2023-03-09 22:28:34 +0530

    Create Slice it.py

---
+++ The Basics/Intro to Strings/Slice it.py
@@ -0,0 +1,9 @@
+"""You are decoding the agent name of a rival nation. Fortunately, the code is simple to crack. The real name can be obtained by extracting the letters from the 3rd position to the 6th position (both inclusive).
+
+Given an agent_name, extract and print the real name.
+
+Complete the given program"""
+
+agent_name='ReaLmEdystProgRammerhvvdvkjhoillhdsiolksdluvsdkjhlkdiufdjkkvdsoajckvuskcnkjasdvkknkdfjkvb'
+
+print(agent_name[2:6])
