15412761f93fc6a64071c7cad0419003a0ff7fb7
santhoshi00 <107908927+santhoshi00@users.noreply.github.com>
2023-03-09 22:23:54 +0530

    Create Convert string to integer.py

---
+++ The Basics/Variables/Convert string to integer.py
@@ -0,0 +1,11 @@
+"""Your street address is mentioned as "220051". However, the online billing system only takes integer as inputs
+
+Store your street address in a string. Convert the string to an integer and print the given integer.
+
+Example Output:
+
+220051"""
+
+string="220051"
+integer_2=int(string)
+print(integer_2)
